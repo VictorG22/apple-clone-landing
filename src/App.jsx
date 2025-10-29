61af873d4275b0096e10ac65faeb06f2f2fb4d72
VictorG22 <victorgaona702@gmail.com>
2025-10-28 23:29:29 -0700

    started to do model of product and finished adding user input of product variations

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Hero from './components/Hero.jsx'
 import Nav from './components/Nav.jsx'
+import ProductViewer from './components/ProductViewer.jsx'
 
 const App = () => {
   return (
     <main>
       <Nav></Nav>
       <Hero></Hero>
+      <ProductViewer></ProductViewer>
     </main>
   )
 }
