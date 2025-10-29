799697b8c7281a68cba95bdff1bd5ce0ba2a3aa1
VictorG22 <victorgaona702@gmail.com>
2025-10-28 22:50:07 -0700

    Adding Hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import Hero from './components/Hero.jsx'
 import Nav from './components/Nav.jsx'
 
 const App = () => {
   return (
     <main>
       <Nav></Nav>
+      <Hero></Hero>
     </main>
   )
 }
