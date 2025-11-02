5b2a3cb0a2d58f1919cfb2226d543968979652ff
VictorG22 <victorgaona702@gmail.com>
2025-11-02 01:41:58 -0700

    Added 3D Product Viewer Section

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
 import Hero from './components/Hero.jsx'
 import Nav from './components/Nav.jsx'
 import ProductViewer from './components/ProductViewer.jsx'
+import gsap from 'gsap'
+import { ScrollTrigger, SplitText } from 'gsap/all'
+
+gsap.registerPlugin(ScrollTrigger)
 
 const App = () => {
   return (
